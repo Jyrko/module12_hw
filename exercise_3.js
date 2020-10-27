f6f060508259832f73fe4a9513bcbd46626503bc
Iuliia Tokarevskaia <iuliia.tokarevskaia@socialbakers.com>
2020-10-27 08:39:29 +0100

    Проверено

--- exercise_3.js
+++ exercise_3.js
@@ -1,7 +1,8 @@
 function createObjWithoutProto() {
-  const obj = {};
-  return Object.setPrototypeOf(obj, null);
+  return Object.create(null);
 }
 
 const tstObj = createObjWithoutProto();
 console.log(Object.getPrototypeOf(tstObj));
+
+// Задание выполнено верно, но можно выполнить чуть короче и создавать объект сразу без прототипа с помощью функции Object.create()
